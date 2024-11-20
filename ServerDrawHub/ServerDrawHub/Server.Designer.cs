e9c7fd5ec08e07c6155fa6a56ff084c26bb6cc08
RibunLoc <hothanhloc12345@gmail.com>
2024-11-20 13:48:34 +0700

    init new

---
+++ ServerDrawHub/ServerDrawHub/Server.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace ServerDrawHub
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Name_port = new System.Windows.Forms.Label();
+            this.tb_input_port = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel_draw = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(1064, 44);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Name_port
+            // 
+            this.Name_port.AutoSize = true;
+            this.Name_port.Font = new System.Drawing.Font("Segoe UI", 11.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Name_port.Location = new System.Drawing.Point(26, 59);
+            this.Name_port.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Name_port.Name = "Name_port";
+            this.Name_port.Size = new System.Drawing.Size(71, 37);
+            this.Name_port.TabIndex = 1;
+            this.Name_port.Text = "Port:";
+            // 
+            // tb_input_port
+            // 
+            this.tb_input_port.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_input_port.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_input_port.Font = new System.Drawing.Font("Segoe UI", 11.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tb_input_port.Location = new System.Drawing.Point(5, 15);
+            this.tb_input_port.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_input_port.Name = "tb_input_port";
+            this.tb_input_port.Size = new System.Drawing.Size(484, 35);
+            this.tb_input_port.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.tb_input_port);
+            this.panel1.Location = new System.Drawing.Point(98, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(493, 64);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.Name_port);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1412, 139);
+            this.panel2.TabIndex = 4;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel_draw
+            // 
+            this.panel_draw.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel_draw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_draw.Location = new System.Drawing.Point(0, 139);
+            this.panel_draw.Name = "panel_draw";
+            this.panel_draw.Size = new System.Drawing.Size(1412, 770);
+            this.panel_draw.TabIndex = 5;
+            this.panel_draw.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1412, 909);
+            this.Controls.Add(this.panel_draw);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.857143F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Server_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Name_port;
+        private System.Windows.Forms.TextBox tb_input_port;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel_draw;
+    }
+}
+
---
+++ ServerDrawHub/ServerDrawHub/Server.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerDrawHub
+{
+    public partial class Server : Form
+    {
+        private TcpListener server;
+        private bool isRun = true;
+        private List<(Point start, Point end)> lines = new List<(Point, Point)>(); // tạo danh sách lưu trữ đường vẽ
+        private Queue<(Point start, Point end)> drawQueue = new Queue<(Point, Point)>();
+        private bool isRunning = false;
+
+        public Server()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Server_Load(object sender, EventArgs e)
+        {
+            Task.Run(() => {
+                ProcessDrawQueue();
+                });
+
+            panel_draw.Paint += panel_draw_Paint;
+        }
+
+        private void StopServer()
+        {
+            isRunning = false;
+            server.Stop();  
+        }
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private async void ProcessDrawQueue()
+        {
+            while(isRunning)
+            {
+                (Point start, Point end)? line = null;
+
+                lock (drawQueue)
+                {
+                    if(drawQueue.Count > 0)
+                    {
+                        line = drawQueue.Dequeue();
+                    }
+                }
+
+                if(line.HasValue)
+                {
+                    DrawLineOnServer(line.Value.start, line.Value.end);
+
+                }
+
+                await Task.Delay(5);    
+            }
+        }
+    
+
+        private async void ListenForClient()
+        {
+            try
+            {
+                while (!isRun)
+                {
+                    TcpClient client = await server.AcceptTcpClientAsync();
+                    ProcessClient(client);
+
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                // Xử lý khi server đã bị ngắt
+            }
+            catch (SocketException)
+            {
+                // Xử lý lỗi socket
+            }
+
+        }
+
+        private async void ProcessClient(TcpClient client)
+        {
+            try
+            {
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] bodem = new byte[1024];
+                    int bytesRead;
+
+                    while ((bytesRead = await stream.ReadAsync(bodem, 0, bodem.Length)) > 0)
+                    {
+                        string data = Encoding.ASCII.GetString(bodem, 0, bytesRead);
+
+                        //tách dữ liệu và xử lý
+                        string[] parts = data.Split(',');
+                        Point start = new Point(
+                            int.Parse(parts[0]) * panel_draw.Width / int.Parse(parts[4]),
+                            int.Parse(parts[1]) * panel_draw.Height / int.Parse(parts[5])
+                            );
+                        Point end = new Point(
+                            int.Parse(parts[2]) * panel_draw.Width / int.Parse(parts[4]),
+                            int.Parse(parts[3]) * panel_draw.Height / int.Parse(parts[5])
+                            );
+
+                        lock (drawQueue)
+                        {
+                            drawQueue.Enqueue((start, end));
+                        }
+
+
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);    
+            }
+
+        }
+
+        private void panel_draw_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(BackColor, 2))
+            {
+                foreach (var line in lines)
+                {
+                    e.Graphics.DrawLine(pen, line.start, line.end);
+                }
+
+            }
+        }
+
+        private void DrawLineOnServer(Point start, Point end)
+        {
+            lines.Add((start, end));
+
+            using (Graphics g = this.panel_draw.CreateGraphics())
+            {
+                g.DrawLine(Pens.Black, start, end);
+            }
+        }
+
+
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // button Listen
+        {
+            if (isRun)
+            {
+                isRun = false;
+                button1.Text = "Stop!";
+                button1.BackColor = Color.White;
+                server = new TcpListener(IPAddress.Any, 5000);
+                tb_input_port.Text = 5000.ToString();
+                server.Start();
+                ListenForClient();
+            }
+            else
+            {
+                isRun = true;
+                button1.Text =  "Listen";
+                button1.BackColor = SystemColors.ActiveBorder;
+                // cần ngắt hệ thống server
+                if (server != null)
+                {
+                    StopServer();
+                    server = null;
+                }
+            }
+            
+        }
+
+        
+    }
+}
